3159c60a6e8d715451ace82ad93b942bf09ea119
Johannes Schumeth <johannes.schumeth@gmail.com>
2021-10-11 00:06:17 +0200

    change prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,27 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new WebpackObfuscator({
+      rotateStringArray: true,
+    }),
+  ],
+  // webpack loader rules array
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'post',
+        use: {
+          loader: WebpackObfuscator.loader,
+          options: {
+            rotateStringArray: true,
+          },
+        },
+      },
+    ],
+  },
 });
